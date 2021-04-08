5a9d16752fcf93c1ad2578fded7914d0bf648559
Alvin Neri <alvin.neri.ece@gmail.com>
2021-04-08 13:37:46 +0800

    add audit page

--- src/features/admin/audit/index.js
+++ src/features/admin/audit/index.js
@@ -3,23 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setCategories } from "../../../store/admin/actions";
 import { db } from "../../../firebase/config";
 import { DownOutlined } from "@ant-design/icons";
-import {
-  Layout,
-  Menu,
-  Dropdown,
-  Input,
-  Button,
-  Form,
-  Row,
-  Col,
-  Popconfirm,
-  Table,
-  Tag,
-  Space,
-} from "antd";
-
-import CurrentMatch from "./CurrentMatch";
-import { toast } from "react-toastify";
+import { Layout, Menu, Dropdown, Table } from "antd";
 import { columns } from "./Columns";
 
 const { Header, Content } = Layout;
@@ -28,11 +12,10 @@ const Audit = () => {
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.admin);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [name, setName] = useState(null);
+
   const [history, setHistory] = useState([]);
-  const [matchNumber, setMatchNumber] = useState("");
+
   const [drawMultiplier, setDrawMultplier] = useState("");
-  const [currentMatch, setCurrentMatch] = useState("");
 
   const [appPercentage, setAppPercentage] = useState(0);
   const [betLimits, setBetLimits] = useState(10000);
@@ -102,6 +85,12 @@ const Audit = () => {
                 doc.data().draw?.totalBets +
                 doc.data().meron?.totalBets,
               id: doc.id,
+              data: {
+                ...doc.data(),
+                id: doc.id,
+                drawMultiplier: drawMultiplier,
+                appPercentage: appPercentage,
+              },
             });
           }
         });
@@ -125,157 +114,6 @@ const Audit = () => {
       })}
     </Menu>
   );
-
-  const deleteMatch = async () => {
-    const categoriesRef = await db
-      .collection("categories")
-      .doc(selectedCategory.id)
-      .update({
-        match: null,
-      })
-      .then(() => {
-        toast.success("Category has no current match.");
-        setName("");
-        setMatchNumber("");
-      });
-  };
-
-  const createNewMatch = async () => {
-    if (!name) {
-      toast.error("Match name is required.");
-      return;
-    }
-    if (!matchNumber) {
-      toast.error("Match Number is required.");
-      return;
-    }
-    if (!selectedCategory) {
-      toast.error("Category is required.");
-      return;
-    }
-
-    const categoriesRef = await db
-      .collection("categories")
-      .doc(selectedCategory.id)
-      .update({
-        match: {
-          name: name,
-          status: "OPEN",
-          result: "PENDING",
-          number: matchNumber,
-          meron: {
-            totalBets: 0,
-            betters: [],
-          },
-          wala: {
-            totalBets: 0,
-            betters: [],
-          },
-          draw: {
-            totalBets: 0,
-            betters: [],
-          },
-        },
-        isProcessed: false,
-      })
-      .then(() => {
-        toast.success("New Match Created.");
-        setName("");
-        setMatchNumber("");
-      });
-  };
-
-  const getPayout = (totalBets, totalSides) => {
-    if (!totalSides) {
-      return 0;
-    }
-    const origMultiplier = (totalBets / totalSides) * 100;
-    console.log(origMultiplier);
-    const percent = appPercentage / 100;
-    const afterCut = origMultiplier - origMultiplier * percent;
-
-    const payout = (totalBets / 100) * afterCut;
-    return afterCut.toFixed(2);
-  };
-
-  const processCredits = async () => {
-    if (!currentMatch) {
-      toast.error("No Bets to be process.");
-      return;
-    }
-
-    if (currentMatch.isProcessed) {
-      toast.error("Bets already processed.");
-      return;
-    }
-
-    if (currentMatch) {
-      await db.collection("fights").add({
-        ...currentMatch.match,
-        date: new Date(),
-        categoryId: selectedCategory.id,
-      });
-
-      const categoriesRef = await db
-        .collection("categories")
-        .doc(selectedCategory.id)
-        .update({
-          isProcessed: true,
-        });
-
-      if (currentMatch.match.result === "MERON") {
-        await currentMatch.match.meron.betters.forEach(async (item, index) => {
-          const userRef = await db.collection("users").doc(item.user);
-          const user = await userRef.get();
-          const credits = user.data()?.credits ? user.data()?.credits : 0;
-
-          await userRef.update({
-            credits:
-              credits +
-              (getPayout(
-                currentMatch?.match?.meron?.totalBets +
-                  currentMatch?.match?.wala?.totalBets,
-                currentMatch?.match?.meron?.totalBets
-              ) /
-                100) *
-                item.amount,
-          });
-        });
-        toast.success("All bets were processed successfully.");
-      } else if (currentMatch.match.result === "WALA") {
-        currentMatch.match.wala.betters.forEach(async (item) => {
-          const userRef = await db.collection("users").doc(item.user);
-          const user = await userRef.get();
-          const credits = user.data()?.credits ? user.data()?.credits : 0;
-
-          await userRef.update({
-            credits:
-              credits +
-              (getPayout(
-                currentMatch?.match?.meron?.totalBets +
-                  currentMatch?.match?.wala?.totalBets,
-                currentMatch?.match?.wala?.totalBets
-              ) /
-                100) *
-                item.amount,
-          });
-        });
-        toast.success("All bets were processed successfully.");
-      } else if (currentMatch.match.result === "DRAW") {
-        currentMatch.match.draw.betters.forEach(async (item) => {
-          const userRef = await db.collection("users").doc(item.user);
-          const user = await userRef.get();
-          const credits = user.data()?.credits ? user.data()?.credits : 0;
-
-          await userRef.update({
-            credits: credits + drawMultiplier * item.amount,
-          });
-        });
-      }
-
-      // deleteMatch();
-    }
-  };
 
   return (
     <Layout style={{ height: "100%" }}>
