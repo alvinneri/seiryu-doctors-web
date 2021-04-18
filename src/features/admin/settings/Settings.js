931e78cd708d20fe0318c248202f784353e41289
Alvin Neri <alvin.neri.ece@gmail.com>
2021-04-19 07:18:45 +0800

    draw

--- src/features/admin/settings/Settings.js
+++ src/features/admin/settings/Settings.js
@@ -24,6 +24,7 @@ export const Settings = () => {
         betMin,
         drawMultiplier,
         sabongTwitchChannel: twitchChannel,
+        drawBetLimit,
       });
     } else {
       const appSettingRef = db.collection("app_settings").add({
@@ -32,6 +33,7 @@ export const Settings = () => {
         betMin,
         drawMultiplier,
         sabongTwitchChannel: twitchChannel,
+        drawBetLimit,
       });
     }
     toast.success("App Settings Updated");
