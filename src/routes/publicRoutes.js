d702cd55bdea5a267aba62c6c891ac2592640c1a
Dann Russell Molina <dannrussell.molina@fligno.com>
2021-04-27 13:31:18 +0800

    created registration

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../features/admin/Home";
 import Login from "../features/auth/Login";
+import Register from "../features/auth/Register";
 import Twitch from "../features/admin/twitch";
 
 export const PublicRoutes = () => {
   return (
     <Switch>
       <Route exact path={"/"} component={Login} />
+      <Route exact path={"/register"} component={Register} />
       <Route path={"/public/twitch"} component={Twitch} />
     </Switch>
   );
