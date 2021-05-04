be34097a261811320ebb98cce91d8479cc757c9f
Alvin Neri <alvin.neri.ece@gmail.com>
2021-05-05 06:56:28 +0800

    recruiter

--- src/features/recruiter/recruited/index.js
+++ src/features/recruiter/recruited/index.js
@@ -8,13 +8,24 @@ import {
 } from "../../../store/recruiter/actions";
 import { CopyFilled } from "@ant-design/icons";
 import { columns } from "./Columns";
+import moment from "moment";
+import { DatePicker, Space } from "antd";
 const { Text } = Typography;
 
 const RecruitedPlayers = () => {
+  const currentDate = moment(new Date()).format("YYYY-MM");
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.public);
   const { recruitedPlayers } = useSelector((state) => state.recruiter);
   const [_users, setUsers] = useState([]);
+  const [email, setEmail] = useState(null);
+  const [selected, setSelectedDate] = useState(currentDate);
+  const monthFormat = "YYYY-MM";
+  const [userDetails, setUserDetails] = useState(null);
+  function onChange(date, dateString) {
+    setSelectedDate(dateString);
+    console.log(date, dateString);
+  }
 
   const getUsers = async () => {
     const usersRef = await db.collection("users");
@@ -27,7 +38,13 @@ const RecruitedPlayers = () => {
         let _totals = 0;
         let docs;
         if (!fightRef.empty) {
-          fightRef.forEach((fight) => {
+          let _fightRef = [...fightRef.docs];
+          let _filteredFights = _fightRef.filter(
+            (item) =>
+              moment(item.data().date.toDate()).format("YYYY-MM") === selected
+          );
+
+          _filteredFights.forEach((fight) => {
             if (fight.result !== "CANCELLED" && fight.result !== "DRAW") {
               fight.data()?.meron?.betters.forEach((meron) => {
                 if (meron.user === doc.data().uid) {
@@ -48,6 +65,38 @@ const RecruitedPlayers = () => {
               });
             }
           });
+
+          if (doc.data().userType === "RECRUITER") {
+            const usersRef = await db.collection("users");
+            const _snapshot = await usersRef
+              .where("invitedBy", "==", doc.data().uid)
+              .get();
+            if (!_snapshot.empty) {
+              _snapshot.forEach((doc2) => {
+                _filteredFights.forEach((fight) => {
+                  if (fight.result !== "CANCELLED" && fight.result !== "DRAW") {
+                    fight.data()?.meron?.betters.forEach((meron) => {
+                      if (meron.user === doc2.data().uid) {
+                        _totals = _totals + parseInt(meron.amount);
+                      }
+                    });
+
+                    fight.data()?.wala?.betters.forEach((wala) => {
+                      if (wala.user === doc2.data().uid) {
+                        _totals = _totals + parseInt(wala.amount);
+                      }
+                    });
+
+                    fight.data()?.draw?.betters.forEach((draw) => {
+                      if (draw.user === doc2.data().uid) {
+                        _totals = _totals + parseInt(draw.amount);
+                      }
+                    });
+                  }
+                });
+              });
+            }
+          }
         }
 
         docs = {
@@ -130,6 +179,18 @@ const RecruitedPlayers = () => {
           COPY REFERRAL REGISTRATION LINK
         </Button>
         <span>{`https://barako-bet.netlify.app/register/${user.uid}`}</span>
+      </div>
+      <Typography style={{ margin: "1em 0em" }}>
+        Subrecruiter of: {userDetails?.invitedBy ? email : "None"}
+      </Typography>
+      <div style={{ display: "flex" }}>
+        <Typography style={{ margin: "1em 1em 1em 0" }}>Filter:</Typography>
+        <DatePicker
+          defaultValue={moment(currentDate, monthFormat)}
+          format={monthFormat}
+          picker="month"
+          onChange={onChange}
+        />
       </div>
 
       <Table
